e455410b08ad692d636a9be5aae399ef6cad9ee1
Никита Никифоров <s02170179@practicum.cs.msu.su>
2019-09-29 11:39:16 +0300

    Решена третья задача

--- task_2/BiSection.py
+++ task_2/BiSection.py
@@ -1,10 +1,21 @@
 from math import *
-import numpy as np
-import readline
+
 func_str = input()
 number_str = input()
 
 a = tuple(number_str.split(","))
-for x in range(int(a[0]), int(a[1])):
-    print(eval(func_str))
+start, end = float(a[0]), float(a[1])
 
+func = eval("lambda x: " + func_str);
+
+mid = (end + start) / 2
+eps = 0.000001
+
+while abs(func(mid)) > eps:
+    if func(start) * func(mid) < 0:
+        end = mid
+    else:
+        start = mid
+    mid = (end + start) / 2
+
+print(mid)
