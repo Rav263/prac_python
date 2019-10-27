6f66da23c1f79d7ea0fa3435b04febd3ff6d03d1
Никита Никифоров <nickiforov.nik@gmail.com>
2019-10-27 13:21:17 +0300

    Решена задача с таблицей умножения

--- task_6/MultTable.py
+++ task_6/MultTable.py
@@ -1,13 +1,14 @@
+from math import ceil
 n, size = eval(input())
 
 max_len = len(str(n * n))
 num_len = len(str(n))
 
 split_str = "=" * size;
-string_len = max_len + num_len * 2 + 8
-string_len = (size + 1) // string_len
+string_len = max_len + num_len * 2 + 9
+string_len = (size + 3) // string_len
+rows = ceil(n / string_len)
 
-print(string_len)
 
 print(split_str)
 strings = []
@@ -16,14 +17,12 @@ for i in range(1, n + 1):
     for j in range(1, n + 1):
         string = f"{i:>{num_len}} * {j:<{num_len}} = {i*j:<{max_len}} "
         strings.append(string)
-for s in range(n // string_len + 1):
+for s in range(rows):
     for a in range(n):
         for b in range(string_len):
             if s * n * string_len + b * n + a >= len(strings):
-                print()
                 continue
-            if b != string_len - 1:
-                print(strings[s * n * string_len + b * n + a], end="|")
-            else:
-                print(strings[s * n * string_len + b * n + a])
+            if b != 0: print("| ", end="")
+            print(strings[s * n * string_len + b * n + a], end="")
+        print()
     print(split_str)
