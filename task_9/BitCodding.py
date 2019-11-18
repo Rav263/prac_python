f46fb537864df0a4fd1f1ad8865c9a94b7906d02
Никита Никифоров <nickiforov.nik@gmail.com>
2019-11-18 17:17:33 +0300

    Fineshed solution

--- task_9/BitCodding.py
+++ task_9/BitCodding.py
@@ -1,34 +1,30 @@
 from collections import Counter
 from math import ceil
 
-#def xehs(string):
-#    byte_string = bytes(string, "ASCII") 
-    #new_list = bytes((now_byte - 32) for now_byte in reversed(byte_string))
-    #new_list = bytes(now_byte >> 2 for now_byte in new_list)
-    #return int.from_bytes(new_list, "little")
 
-#    return sum(((byte_string[i]-32) << i*6) for i in range(len(byte_string)))
 def xehs(s):
-    n = 0
-    
+    a = []
+
     for c in s:
-        d = ord(c) - 32
-        n = n*64 | d
+        d = ord(c)-32
+        a.extend([chr(48+(d>>3)),chr(48+(d&7))])
 
-    return n
+    return int("".join(a), 8)
 
-
-
-
-def shex(number):
-    list_of_bytes = list()
+def shex(n):
+    preoctal = oct(n)
+    N = len(preoctal)
     
-    while number > 0:
-        list_of_bytes.append(32 + (number & 63))
-        number >>= 6
-    
-    return bytes(reversed(list_of_bytes)).decode("ASCII")
+    octal = "0"*(N%2) + preoctal[2:]
+    N = len(octal)
 
+    a = []
+    for i in range(0,N,2):
+        d1 = ord(octal[i]) - 48
+        d2 = ord(octal[i+1]) - 48
+        a.append(chr(d1*8 + d2 + 32))
+
+    return "".join(a)
 
 def encode(string):
     str_len = len(string)
@@ -39,12 +35,23 @@ def encode(string):
     bit_string = "".join(dictionary_of_codes[i] for i in string)
     bit_string += "0" * (ceil(len(bit_string) / 6) * 6 - len(bit_string))
     
-    return (str_len, "".join(dictionary_of_codes.keys()), shex(int(bit_string, 2)).strip())
+    index_of_one = bit_string.find("1")
+    index_of_one += 6 - index_of_one % 6
+
+    coded_string = "".join(" " if bit_string[i:i+6] == "000000" else shex(int(bit_string[i:i+6], 2)) for i in range(0, index_of_one, 6)) 
+    coded_string += shex(int(bit_string[index_of_one:], 2))
+    return (str_len, "".join(dictionary_of_codes.keys()), coded_string)
 
 def decode(str_len, code, coded_string):
     dictionary_of_codes = dict(list(("1" * i + "0", code[i]) for i in range(len(code))))
-    string_list_of_codes = (str(bin(xehs(coded_string)))[2:] + "0").replace("0", "0 ").split()[:str_len]
-    decoded_string = "".join(dictionary_of_codes[i] for i in string_list_of_codes)
     
+    num_of_spaces = 0
+    for i in coded_string:
+        if i != " ":
+            break
+        num_of_spaces += 1
+    
+    string_list_of_codes = ((lambda x: "000000"*num_of_spaces+"0" * ((6 - len(x) % 6) % 6) + x)(str(bin(xehs(coded_string)))[2:])).split("0")[:-1][:str_len]
+    decoded_string = "".join(dictionary_of_codes[i + "0"] for i in string_list_of_codes)
 
     return decoded_string
