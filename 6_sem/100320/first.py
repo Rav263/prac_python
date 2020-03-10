52628b2f3385a74f4bb5ea0aaa53a44fb03a036b
Никита Никифоров <nickiforov.nik@gmail.com>
2020-03-10 09:40:43 +0300

    Add scaleble speed and abstract coords

--- 6_sem/100320/first.py
+++ 6_sem/100320/first.py
@@ -1,26 +1,40 @@
-import sys, pygame
+import sys, pygame, math
 pygame.init()
 
-size = width, height = 1280, 720
+screen_size = width, height = 1280, 720
+size = width / 0.1, height / 0.1
+DX, DY = 0.1, 0.1
 speed = [2, 2]
 black = 0, 0, 0
 
-screen = pygame.display.set_mode(size)
+
+def convert(x, y):
+    return (math.floor(x * DX) , math.floor(y * DY))
+
+screen = pygame.display.set_mode(screen_size)
 
 ball = pygame.image.load("intro_ball.gif")
-ballrect = ball.get_rect()
+x, y = size[0] / 2, size[1] / 2
+
+ball_rect = ball.get_rect();
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
+    x, y = x + speed[0], y + speed[1]
+
+    new_coords = convert(x, y)
+    #print(x, y, new_coords)
+
+    ball_rect.center = new_coords
+
+    if ball_rect.left < 0 or ball_rect.right > width:
         speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
+    if ball_rect.top < 0 or ball_rect.bottom > height:
         speed[1] = -speed[1]
 
     screen.fill(black)
-    screen.blit(ball, ballrect)
+    screen.blit(ball, ball_rect)
     pygame.display.flip()
 
